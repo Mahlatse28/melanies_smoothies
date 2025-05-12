d73921de66e8fff158ad47b9c362da021a60f140
Mahlatse28 <mahlatse.masemola@mukuru.com>
2025-05-12 16:14:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,5 @@ if not name_on_order and st.button('Submit Order'):
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response. json())
+# st.text(smoothiefroot_response. json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
